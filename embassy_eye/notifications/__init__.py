9d18c35858422658482812a1ae9bd41b0c325309
a-s2pac <aleksandr.stupak@offsidegaming.com>
2025-11-25 22:07:26 +0100

    fixed healthcheck

--- embassy_eye/notifications/__init__.py
+++ embassy_eye/notifications/__init__.py
@@ -6,7 +6,23 @@ from .telegram import (
     send_result_notification,
     send_telegram_message,
     send_telegram_document,
+    send_healthcheck_message,
+    send_healthcheck_slots_found,
+    send_healthcheck_slot_busy,
+    send_healthcheck_ip_blocked,
+    send_healthcheck_reloaded_page,
+    get_ip_and_country,
 )
 
-__all__ = ["send_result_notification", "send_telegram_message", "send_telegram_document"]
+__all__ = [
+    "send_result_notification",
+    "send_telegram_message",
+    "send_telegram_document",
+    "send_healthcheck_message",
+    "send_healthcheck_slots_found",
+    "send_healthcheck_slot_busy",
+    "send_healthcheck_ip_blocked",
+    "send_healthcheck_reloaded_page",
+    "get_ip_and_country",
+]
 
